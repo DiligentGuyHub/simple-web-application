62fd5af7a24f2d6e2e8a5f8a605eef163d9b15b7
WolfStorm <longredtie@yandex.ru>
2021-04-10 03:08:07 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Example.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationContext db;
+        public HomeController(ApplicationContext context)
+        {
+            db = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.Users.ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        // CREATE
+        public async Task<IActionResult> Create(User user)
+        {
+            user.RegistrationDate = DateTime.Now;
+            user.LastSeenOnline = DateTime.Now.ToLocalTime();
+            user.Status = "Active";
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        // DETAILS
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id != null)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+
+        // EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(User user)
+        {
+            db.Users.Update(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        // DELETE
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                {
+                    db.Users.Remove(user);
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            return NotFound();
+        }
+    }
+}
